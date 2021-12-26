d5180d9636cf22501815b3a65b4d4eb436e8b934
Felipe Dos Santos Conceição <felipe.conceicao@mercadolivre.com>
2021-12-26 20:35:42 -0300

    inicial

---
+++ src/test/java/com/br/meli/bootcamp/obterdiploma/obterdiploma/ObterDiplomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.meli.bootcamp.obterdiploma.obterdiploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObterDiplomaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
